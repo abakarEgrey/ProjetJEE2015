f4e8895b23a9418ee7e53c475aa66b4bbc87bbd5
abakarEgrey <egreabakar@yahoo.fr>
2015-04-24 15:28:03 +0200

    complement de la classe Musee

--- grails-app/domain/projetjee2015/Musee.groovy
+++ grails-app/domain/projetjee2015/Musee.groovy
@@ -2,6 +2,12 @@ package projetjee2015
 
 class Musee {
 
+    String nom
+    String horairesOuverture
+    String telephone
+    String accesMetro
+    String accesBus
+
     static constraints = {
     }
 }
