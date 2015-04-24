c7294afbc15f988925d65c9808eea14f8de554b4
abakarEgrey <egreabakar@yahoo.fr>
2015-04-24 16:04:27 +0200

    tests suite

--- grails-app/domain/projetjee2015/DemandeVisiteMusee.groovy
+++ grails-app/domain/projetjee2015/DemandeVisiteMusee.groovy
@@ -1,7 +1,13 @@
 package projetjee2015
 
 class DemandeVisiteMusee {
+    Date dateDemande;
 
     static constraints = {
+        musees(blank: false, nullable: false)
+        demandes(blank: false, nullable: false)
+        dateDemande(blank: false, nullable: false)
     }
+
+    static hasMany = [musees: Musee, demandes: DemandeVisite]
 }
--- grails-app/domain/projetjee2015/Musee.groovy
+++ grails-app/domain/projetjee2015/Musee.groovy
@@ -9,5 +9,14 @@ class Musee {
     String accesBus
 
     static constraints = {
+        nom(blank: false, nullable: false)
+        horairesOuverture(blank: false, nullable: false)
+        telephone(blank: false, nullable: false)
+        accesMetro(blank: false, nullable: false)
+        accesBus(blank: false, nullable: false)
     }
+
+    static hasOne = [adresse: Adresse]
+
+    static hasMany = [demandesVisites: DemandeVisite]
 }
