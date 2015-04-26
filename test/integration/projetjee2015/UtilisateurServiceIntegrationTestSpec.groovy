3af45886516e73053bc36e459d6844f7d47d75d7
Sebastien Gadois <sgadois@gmail.com>
2015-04-26 23:35:08 +0200

    update test

--- test/integration/projetjee2015/UtilisateurServiceIntegrationTestSpec.groovy
+++ test/integration/projetjee2015/UtilisateurServiceIntegrationTestSpec.groovy
@@ -37,13 +37,14 @@ class UtilisateurServiceIntegrationTestSpec extends Specification {
     void "test addToFavorite"() {
         List<Musee> museeList = new ArrayList<>()
         given: "Un utilisateur et musee "
-        Adresse uneAdresse = new Adresse(numero: 2, rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "TOULOUSE")
+        Adresse uneAdresse = new Adresse(numero: 2, rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "TOULOUSE").save()
+        Gestionnaire unGestionnaire = new Gestionnaire(nom: "gestionnaire").save()
         Musee uneMusee =
                 new Musee(nom: "ARCHIVES MUNICIPALES TOULOUSE",
                         horairesOuverture: "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30 pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet.",
                         telephone: "05 61 61 63 33",
-                        accesMetro: "Roseraie (A)", accesBus: "36, 38", gestionnaire: Mock(Gestionnaire),
-                        adresse: uneAdresse)
+                        accesMetro: "Roseraie (A)", accesBus: "36, 38", gestionnaire: unGestionnaire,
+                        adresse: uneAdresse).save()
 
         Utilisateur unUtilisateur = new Utilisateur(nom: "DUPONT", prenom: "Pascal", email: "dupont_pascal@email.fr", sexe: "M", dateNaissance: new Date(1972, 6, 17))
         unUtilisateur.musees = museeList;
@@ -61,20 +62,21 @@ class UtilisateurServiceIntegrationTestSpec extends Specification {
     void "test addDemandeVisite"() {
         List<Musee> museeList = new ArrayList<>()
         given: "Un utilisateur et demandeVisite "
-        Adresse uneAdresse = new Adresse(numero: 2, rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "TOULOUSE")
-        Adresse uneAdresse1 = new Adresse(numero: 5, rue: "RUE SAINT PANTALEON", codePostal: 31000, ville: "TOULOUSE")
+        Adresse uneAdresse = new Adresse(numero: 2, rue: "RUE DES ARCHIVES", codePostal: 31500, ville: "TOULOUSE").save()
+        Adresse uneAdresse1 = new Adresse(numero: 5, rue: "RUE SAINT PANTALEON", codePostal: 31000, ville: "TOULOUSE").save()
+        Gestionnaire unGestionnaire = new Gestionnaire(nom: "gestionnaire").save()
         Musee uneMusee =
                 new Musee(nom: "ARCHIVES MUNICIPALES TOULOUSE",
                         horairesOuverture: "Ouvert du lundi au vendredi de 9h à 17h. Fermeture de 12h à 13h30 pendant toutes les vacances scolaires. Fermeture annuelle la dernière quinzaine de juillet.",
                         telephone: "05 61 61 63 33",
-                        accesMetro: "Roseraie (A)", accesBus: "36, 38", gestionnaire: Mock(Gestionnaire),
+                        accesMetro: "Roseraie (A)", accesBus: "36, 38", gestionnaire: unGestionnaire,
                         adresse: uneAdresse)
 
         Musee uneMusee1 =
                 new Musee(nom: "CMAV - CENTRE MERIDIONAL DE L'ARCHITECTURE DE LA VILLE",
                         horairesOuverture: "Ouvert du mardi au samedi de 13h à 19hfermé les dimanches, jours fériés et du 1er au 15 août",
                         telephone: "05 61 23 30 49",
-                        accesMetro: "Capitole (A), Jean Jaurès (B)", accesBus: "ncv, 2, 10, 12, 14, 38, 78 et 80", gestionnaire: Mock(Gestionnaire),
+                        accesMetro: "Capitole (A), Jean Jaurès (B)", accesBus: "ncv, 2, 10, 12, 14, 38, 78 et 80", gestionnaire: unGestionnaire,
                         adresse: uneAdresse1)
 
         Utilisateur unUtilisateur = new Utilisateur(nom: "DUPONT", prenom: "Pascal", email: "dupont_pascal@email.fr", sexe: "M", dateNaissance: new Date(1972, 6, 17))
